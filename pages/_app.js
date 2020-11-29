fb14e60f2cc29c772283f434b02491e0bd142760
Akshay Ashok <aks28id@gmail.com>
2020-11-29 11:51:57 +0530

    initial project setup done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/index.css'
+import "@/assets/styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Nav from '../components/nav'
+import Nav from "@/components/nav";
 
 export default function IndexPage() {
   return (
