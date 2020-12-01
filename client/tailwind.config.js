00320c15666eec407b85553847f9de440e0f3f1d
Akshay Ashok <aks28id@gmail.com>
2020-12-01 23:09:52 +0530

    tailwind config updated to use more colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+    },
     extend: {
       maxWidth: {
         16: "4rem",
