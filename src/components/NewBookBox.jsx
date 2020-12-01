41d578f9bad0336e11f5684c7a582d49f193cfcc
Akshay Ashok <aks28id@gmail.com>
2020-12-01 20:14:45 +0530

    textarea for newBookBox

--- src/components/NewBookBox.jsx
+++ src/components/NewBookBox.jsx
@@ -7,13 +7,18 @@ const NewBookBox = () => {
       <p className="mt-3 text-lg">Paul Graham</p>
       <div className="mt-4 flex">
         <img
-        className="w-24"
+          className="w-24"
           src="https://books.google.com/books/content?id=shycAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
           alt="book"
         />
-        <p className="ml-6">
-            Really great book, you keep it.
-        </p>
+        <textarea
+          className="w-full mt-4 ml-6 border-2 border-gray-300 bg-gray-50border-gray-200 text-gray-900 px-2 py-2 no-outline"
+          placeholder="Some comments about this book. Max 280 chars"
+          name="desc"
+          id="desc"
+          rows="2"
+          maxLength="280"
+        ></textarea>
       </div>
     </div>
   );
