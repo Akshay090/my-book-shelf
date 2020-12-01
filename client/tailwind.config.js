27ec8696ae5019e1efb1d50cdec0653717f163a2
Akshay Ashok <aks28id@gmail.com>
2020-12-01 22:08:29 +0530

    tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media", // 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        "accent-1": "#333",
-      },
       maxWidth: {
         16: "4rem",
         48: "12rem",
