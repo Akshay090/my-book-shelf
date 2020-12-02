315506b5d17e50b056dcd2bb691e2ee26395a825
Tulsi Prasad <tulsi.prasad50@gmail.com>
2020-12-02 13:54:15 +0530

    fix: set darkMode to class tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     colors: {
       ...colors,
