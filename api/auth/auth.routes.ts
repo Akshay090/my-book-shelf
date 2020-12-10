ea34c72f0f8f5b752ad0944f4dd2261fbb666183
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-12-10 07:43:04 +0530

    feat(server/oauth): add routes

--- api/auth/auth.routes.ts
+++ api/auth/auth.routes.ts
@@ -1,28 +1,58 @@
 import { Request, Response, NextFunction, Router } from "express";
 
 import validateQuery from "../middlewares/validate-query";
-import { dummySchema, dummyType } from "./auth.schema";
-import { dummyService } from "./auth.service";
+import {
+  OAuthGenerate,
+  getAccessToken,
+  getUserInfo,
+  generateJwt,
+} from "./auth.service";
+import { oauthCallback, oauthCallbackSchema } from "./auth.schema";
 
 const router: Router = Router();
 
-const handlePostTest = async (
+const handleGetGenerate = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const { dummyData } = req.body as dummyType;
-    const result = await dummyService({ dummyData });
-    res.json({
-      success: true,
-      message: "dummyService was executed",
-    });
+    const OAuthToken = await OAuthGenerate();
+    res.redirect(
+      `https://api.twitter.com/oauth/authorize?oauth_token=${OAuthToken}`
+    );
   } catch (err) {
     next(err);
   }
 };
 
-router.post("/test", validateQuery("body", dummySchema), handlePostTest);
+const handleGetCallback = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { oauth_token, oauth_verifier } = req.query as oauthCallback;
+    const accessToken = await getAccessToken(oauth_token, oauth_verifier);
+    const userInfo = await getUserInfo(accessToken);
+    const token = await generateJwt(userInfo);
+    const tokenParam = Buffer.from(token).toString("base64");
+    if (process.env.NODE_ENV === "production")
+      res.redirect(
+        `${process.env.API_HOSTNAME}/user/dashboard?token=${tokenParam}`
+      );
+    else
+      res.redirect(`http://localhost:3000/user/dashboard?token=${tokenParam}`);
+  } catch (err) {
+    next(err);
+  }
+};
+
+router.get("/auth/generate", handleGetGenerate);
+router.get(
+  "/auth/callback",
+  validateQuery("query", oauthCallbackSchema),
+  handleGetCallback
+);
 
 export default router;
