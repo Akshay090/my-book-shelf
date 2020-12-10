dcc77e37e3dca0b90ce977a2a9d873949e3112c7
Akshay Ashok <aks28id@gmail.com>
2020-12-10 19:48:17 +0530

    fix(client): tailwind prod utils, jsconfig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     colors: {
